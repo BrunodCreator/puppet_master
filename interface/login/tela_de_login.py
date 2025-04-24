6abddbfe2148a22e07c51c15d096f72d88a486a4
BrunodCreator <bruno.dqueirozmt@gmail.com>
2025-04-24 15:20:32 -0300

    Foi criado o frame da tela de login com o campo para usuário e senha

--- interface/login/tela_de_login.py
+++ interface/login/tela_de_login.py
@@ -1,1 +1,41 @@
-class
+from interface.tela_base import TelaBase
+import customtkinter as ctk
+
+
+class TelaLogin(TelaBase):
+    def __init__(self):
+        super().__init__()
+
+        self.alterar_titulo('ＬＯＧＩＮ')
+
+        # Criar um frame para o login (com fundo e um pouco de espaço)
+        self.frame_login = ctk.CTkFrame(self.janela, fg_color="gray20", width=600, height=400)
+        self.frame_login.place(relx=0.5, rely=0.5, anchor="center")  # Centraliza o frame na tela
+
+        # Criar o label e o campo de entrada para o usuário
+        self.label_usuario = ctk.CTkLabel(self.frame_login, text="Usuário:")
+        self.label_usuario.grid(row=0, column=0, padx=6, pady=3, sticky="w")  # Alinha à esquerda
+
+        self.entry_usuario = ctk.CTkEntry(self.frame_login, width=300)
+        self.entry_usuario.grid(row=1, column=0, padx=6, pady=3, sticky="ew")  # Preenche a largura da coluna
+
+        # Criar o label e o campo de entrada para a senha
+        self.label_senha = ctk.CTkLabel(self.frame_login, text="Senha:")
+        self.label_senha.grid(row=2, column=0, padx=6, pady=3, sticky="w")
+
+        self.entry_senha = ctk.CTkEntry(self.frame_login, show="*")
+        self.entry_senha.grid(row=3, column=0, padx=6, pady=3, sticky="ew")  # Preenche a largura da coluna
+
+        # Criar o botão de login
+        self.botao_login = ctk.CTkButton(self.frame_login, text="Login", command=self.realizar_login)
+        self.botao_login.grid(row=4, column=0, padx=10, pady=20, sticky="ew")
+
+    def realizar_login(self):
+        usuario = self.entry_usuario.get()
+        senha = self.entry_senha.get()
+        print(f"Usuário: {usuario}, Senha: {senha}")
+
+
+if __name__ == '__main__':
+    login = TelaLogin()
+    login.exibir()
--- interface/tela_base.py
+++ interface/tela_base.py
@@ -1,14 +1,21 @@
 import customtkinter as ctk
-
+import os
 
 class TelaBase:
     def __init__(self):
         self.janela = ctk.CTk()
+        #Caminho do diretório onde o script atual está localizado
+        self.pasta_interface = os.path.dirname(os.path.abspath(__file__))
+        #Construir o caminho absoluto para o ícone
+        icone_path = os.path.join(self.pasta_interface, 'puppet_master.ico')
         self.janela.geometry('1000x600')
         self.janela.title("ＰＵＰＰＥＴ   ＭＡＳＴＥＲ")
-        #self.janela.title(f'PUPPET MASTER')
-        #self.janela.title(f'PUPPET MASTER - {nome_janela}')
-        self.janela.iconbitmap('puppet_master.ico')
+        self.janela.iconbitmap(icone_path)
+
+    def alterar_titulo(self, nome_da_janela):
+        """Métodos para alterar o título da janela dinamincamente"""
+        self.janela.title(f'ＰＵＰＰＥＴ   ＭＡＳＴＥＲ   -->   {nome_da_janela}')
+
 
 
     def exibir(self):
